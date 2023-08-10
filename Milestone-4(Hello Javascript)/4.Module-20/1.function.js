ce6e6a8a7bf3474f732c95cf980e48080a9ead22
Tutul Kabir <tutulkabir@gmail.com>
2023-08-10 21:04:48 +0530

    module 20 added

---
+++ Milestone-4(Hello Javascript)/4.Module-20/1.function.js
@@ -0,0 +1,18 @@
+// function declaration
+
+function startFunc() {
+  console.log("Stand up");
+  console.log("walk towards the switch");
+  console.log("Press the switch");
+}
+
+// call the function
+
+startFunc();
+
+console.log("Wake up in the morning");
+startFunc();
+
+console.log("Eating lunch");
+console.log("watching js tutorial");
+startFunc();
---
+++ Milestone-4(Hello Javascript)/4.Module-20/2.parameter.js
@@ -0,0 +1,24 @@
+// parameter in function
+
+function bringSingara(money) {
+  console.log("mama singara den ", money);
+  //   console.log(money);
+}
+
+// bringSingara(100);
+var taka = 100;
+bringSingara(taka);
+
+// function add(num1, num2) {
+//   var sum = num1 + num2;
+//   console.log(sum);
+// }
+
+// add(20, 30);
+
+function sum(num1, num2, num3, num4) {
+  var sum = num1 + num2 + num3 + num4;
+  console.log(sum);
+}
+
+add(20, 30, 50, 60);
