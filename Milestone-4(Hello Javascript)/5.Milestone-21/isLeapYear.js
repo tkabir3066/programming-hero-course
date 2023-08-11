2cc6f41dd308f0f182427344c76c7413df67f2ff
Tutul Kabir <tutulkabir@gmail.com>
2023-08-11 16:30:39 +0530

    practice problem 1 addded

--- Milestone-4(Hello Javascript)/5.Milestone-21/isLeapYear.js
+++ Milestone-4(Hello Javascript)/5.Milestone-21/isLeapYear.js
@@ -1,3 +1,5 @@
+// checking Leap year or not
+
 function isLeapYear(year) {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return "Leap Year";
---
+++ Milestone-4(Hello Javascript)/5.Milestone-21/practice problem/problem1.js
@@ -0,0 +1,18 @@
+//1. Write a function findOddSum() that will take the array [5,7,8,10,45,30] as the input parameter and will return the sum of the odd numbers
+
+function findOddSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    if (num % 2 !== 0) {
+      sum = sum + arr[i];
+    }
+  }
+
+  return sum;
+}
+
+const numbers = [5, 7, 8, 10, 45, 30];
+
+const result = findOddSum(numbers);
+console.log(result); //57
