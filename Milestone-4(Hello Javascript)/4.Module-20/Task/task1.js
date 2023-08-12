5ece9ae1be0c73e251d5c00e56f3e699da11f712
Tutul Kabir <tutulkabir@gmail.com>
2023-08-12 10:14:49 +0530

    modified in module 21

--- Milestone-4(Hello Javascript)/4.Module-20/Task/task1.js
+++ Milestone-4(Hello Javascript)/4.Module-20/Task/task1.js
@@ -1,11 +1,14 @@
 //1. Write a function called foo() which prints "foo", and a function called bar() which prints "bar". Call function bar() in the foo() function after printing. What will be the output? Now call the foo() to see the output
 
-function foo() {
-  console.log("foo");
-  bar();
-}
 function bar() {
   console.log("bar");
 }
 
+function foo() {
+  console.log("foo");
+  bar();
+}
+
 foo();
+// foo
+// bar
