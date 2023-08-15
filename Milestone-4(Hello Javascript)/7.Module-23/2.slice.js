f8d6af683a81641912e71260c378c8dc921c056a
Tutul Kabir <tutulkabir@gmail.com>
2023-08-15 17:58:54 +0530

    modified in slice.js file

--- Milestone-4(Hello Javascript)/7.Module-23/2.slice.js
+++ Milestone-4(Hello Javascript)/7.Module-23/2.slice.js
@@ -1,6 +1,10 @@
 // slice.js
 //The slice() method of Array instances returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
 
+// Returns a copy of a section of an array.
+// For both start and end, a negative index can be used to indicate an offset from the end of the array.
+//For example, -2 refers to the second to last element of the array.
+
 const friends = [12, 13, 14, 16, 21, 23, 34, 45, 54, 66];
 
 const partial = friends.slice(5, 7);
