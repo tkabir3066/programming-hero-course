bcaac8f6225b39de3ceac65666e8a6635492afff
Tutul Kabir <tutulkabir@gmail.com>
2023-08-15 18:35:36 +0530

    approach-2 added

--- Milestone-4(Hello Javascript)/7.Module-23/4.duplicter.js
+++ Milestone-4(Hello Javascript)/7.Module-23/4.duplicter.js
@@ -13,11 +13,25 @@ const names = [
   "sabul",
 ];
 
-function removeDuplicate(names) {
+//// approach-1
+/* function removeDuplicate(names) {
   let unique = [];
   for (let i = 0; i < names.length; i++) {
     const name = names[i];
     if (unique.includes(name) === false) {
+      unique.push(name);
+    }
+  }
+
+  return unique;
+} */
+
+// approach-2
+function removeDuplicate(names) {
+  let unique = [];
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    if (unique.indexOf(name) === -1) {
       unique.push(name);
     }
   }
