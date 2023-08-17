e2e5b1ba40505110e68616f830e3b6b8f32428e5
Tutul Kabir <tutulkabir@gmail.com>
2023-08-17 20:36:26 +0530

    dom intro

---
+++ Milestone-4(Hello Javascript)/10.Module-24-5/1.simple.js
@@ -0,0 +1,18 @@
+//1+2+3+4+5
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// 5+4+3+2+1
+
+let sum = 0;
+
+for (let i = 5; i >= 1; i--) {
+  sum = sum + i;
+}
+
+console.log(sum);
